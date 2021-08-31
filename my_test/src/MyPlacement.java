02d213ba58d4606ae97e72f46248e99b099f675d
chaoran lu <luchaorancn@126.com>
2021-08-31 23:25:28 +0200

    try to find unplaced cells

--- my_test/src/MyPlacement.java
+++ my_test/src/MyPlacement.java
@@ -1,4 +1,11 @@
+import com.xilinx.rapidwright.design.Cell;
 import com.xilinx.rapidwright.design.Design;
+import com.xilinx.rapidwright.design.SiteInst;
+import com.xilinx.rapidwright.device.Device;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class MyPlacement {
 
@@ -10,10 +17,26 @@ public class MyPlacement {
 
         String filename = "1_spi.dcp";
         Design design = Design.readCheckpoint("my_test/dcpfile/input/" + filename);
-
         // or if the EDIF inside the DCP is encrypted because of source references,
         //   you can alternatively supply a separate EDIF
         //Design design = Design.readCheckpoint("my_test/dcpfile/input/1_spi.dcp", "my_test/dcpfile/input/1_spi.edf");
+
+        Device device = design.getDevice();
+        Collection<Cell> cells = design.getCells();
+
+        /*to find unplaced Cells*/
+        HashSet<Cell> unplacedCells = new HashSet<>();
+        for (Cell c : cells) {
+            if (!c.isPlaced()) unplacedCells.add(c);
+        }
+
+        /*to find all unoccupied positions*/
+        //HashSet<Cell> unoccupiedPositions = design.;
+
+        //Collection<SiteInst> sites = design.getSiteInsts();
+
+        //BlockPlacer2 bp2 = new BlockPlacer2();
+        //bp2.placeDesign(design,);
 
 
 
