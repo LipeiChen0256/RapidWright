1e158c465efb6c4e372767f97d3eb21390e2df04
chaoran lu <luchaorancn@126.com>
2021-09-09 18:00:37 +0200

    second Commit - realize SA-Algorithm

--- my_test/src/MyPlacement.java
+++ my_test/src/MyPlacement.java
@@ -82,6 +82,13 @@ public class MyPlacement {
                 AllSitesOnDevice.put(s.getName(), s);
             }
         }
+        HashSet<Site> unusedSites = new HashSet<>();
+//        for (Tile t : tiles) {
+//            Site[] tempArr = t.getSites();
+//            for (Site s : tempArr){
+//                if() unusedSites.add(s);
+//            }
+//        }
 
         EDIFNetlist netlist = design.getNetlist();
         EDIFCell topCell = netlist.getTopCell();
