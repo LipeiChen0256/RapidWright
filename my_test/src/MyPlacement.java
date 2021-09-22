e91f7ad83cb6e12c572978d3f18bc41f84bc81a8
chaoran lu <luchaorancn@126.com>
2021-09-23 00:14:20 +0200

    realize random placement

--- my_test/src/MyPlacement.java
+++ my_test/src/MyPlacement.java
@@ -1,16 +1,22 @@
 import com.xilinx.rapidwright.design.*;
 import com.xilinx.rapidwright.design.Module;
 import com.xilinx.rapidwright.device.*;
-import com.xilinx.rapidwright.edif.EDIFCell;
-import com.xilinx.rapidwright.edif.EDIFNetlist;
+import com.xilinx.rapidwright.edif.*;
 import com.xilinx.rapidwright.interchange.DeviceResources;
+import com.xilinx.rapidwright.interchange.SiteSitePIP;
 import com.xilinx.rapidwright.ipi.BlockCreator;
 import com.xilinx.rapidwright.placer.blockplacer.BlockPlacer;
+import com.xilinx.rapidwright.placer.blockplacer.BlockPlacer2;
+import com.xilinx.rapidwright.placer.blockplacer.Path;
+import com.xilinx.rapidwright.placer.blockplacer.PathPort;
 import com.xilinx.rapidwright.router.Router;
 import com.xilinx.rapidwright.tests.CodePerfTracker;
 import com.xilinx.rapidwright.util.FileTools;
 import com.xilinx.rapidwright.util.StringTools;
+import com.xilinx.rapidwright.util.Utils;
+import org.python.modules._hashlib;
 
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class MyPlacement {
@@ -65,71 +71,111 @@ public class MyPlacement {
 
         MyPlacement mp = new MyPlacement();
 
-        String filename = "1_spi.dcp";
-        Design design = Design.readCheckpoint("my_test/dcpfile/input/" + filename);
+        Design design = Design.readCheckpoint("my_test/rapidwright_benchmarks_unrouted_v2/benchmarks/spmc/mjpeg_5cores/configuration_placed.dcp");
         // or if the EDIF inside the DCP is encrypted because of source references,
         // you can alternatively supply a separate EDIF
-//        Design design = Design.readCheckpoint("my_test/dcpfile/input/1_spi.dcp", "my_test/dcpfile/input/1_spi.edf");
+        //Design design = Design.readCheckpoint("my_test/dcpfile/input/1_spi.dcp", "my_test/dcpfile/input/1_spi.edf");
+        /* fill all data structures by itself from the EDIF netlist*/
+        DesignTools.createMissingSitePinInsts(design);
+        design.unrouteDesign();
+        design.getSiteInsts().stream()
+                .filter(siteInst -> Utils.isModuleSiteType(siteInst.getSiteTypeEnum()))
+                .forEach(SiteInst::unPlace);
 
         Device device = design.getDevice();
-        Collection<Cell> cells = design.getCells();
-        Collection<SiteInst> SiteInsts = design.getSiteInsts();
-        Collection<Tile> tiles = device.getAllTiles();
-
-        /*all Sites on this device*/
-        HashMap<String, Site> AllSitesOnDevice = new HashMap<>();
-        for (Tile t : tiles) {
+        //to find all Sites on this device
+        HashMap<String, Site> allSitesOnDevice = new HashMap<>();
+        for (Tile t : device.getAllTiles()) {
             Site[] tempArr = t.getSites();
             for (Site s : tempArr) {
-                AllSitesOnDevice.put(s.getName(), s);
+                allSitesOnDevice.put(s.getName(), s);
             }
         }
 
-        EDIFNetlist netlist = design.getNetlist();
-        EDIFCell topCell = netlist.getTopCell();
         Collection<Net> nets = design.getNets();
-        HashMap<String, ModuleInst> moduleInsts = design.getModuleInstMap();
 
+        /*****************to find all SiteInsts to be placed*******************/
+        Collection<SiteInst> siteInstsToBePlaced = design.getSiteInsts();
 
-        /*to find unplaced Cells*/
-        HashSet<Cell> unplacedCells = new HashSet<>();
-        for (Cell c : cells) {
-            if (!c.isPlaced()) unplacedCells.add(c);
-        }
-        
-        /*to find all Sites that can be populated with a specific SiteInst*/
-        HashMap<SiteTypeEnum, ArrayList<Site>> sitesOfAllTypeOfSI = new HashMap<>();
-        for (SiteInst si : SiteInsts) {
-            SiteTypeEnum t = si.getSiteTypeEnum();
-            if (!sitesOfAllTypeOfSI.containsKey(t)) {
-                sitesOfAllTypeOfSI.put(t, new ArrayList<>());
+        /******************************************************************/
+
+        /**********find all unused Sites that can be populated with needed Type**********/
+        HashMap<SiteTypeEnum, ArrayList<Site>> unusedSitesMapOfNeededType = new HashMap<>();
+
+        for (SiteInst siteInst : siteInstsToBePlaced) {
+            SiteTypeEnum t = siteInst.getSiteTypeEnum();
+            if (!unusedSitesMapOfNeededType.containsKey(t)) {
+                ArrayList<Site> siteList = new ArrayList<>();
+                unusedSitesMapOfNeededType.put(t, siteList);
                 Site[] sites = device.getAllCompatibleSites(t);
                 for (Site s : sites) {
-                    sitesOfAllTypeOfSI.get(t).add(s);
+                    if(!design.isSiteUsed(s)) {
+                        unusedSitesMapOfNeededType.get(t).add(s);
+                    }
                 }
             }
         }
+
+        /**************************************************************************/
+
+        /*******************make a random placement of the design******************/
+        for (SiteInst si: siteInstsToBePlaced){
+            SiteTypeEnum type = si.getSiteTypeEnum();
+            Site site;
+            do{
+                site = unusedSitesMapOfNeededType.get(type).remove(0);
+            }while(design.isSiteUsed(site));
+            si.place(site);
+        }
+        /**************************************************************************/
+
+
+        Collection<Net> nets2 = design.getNets();
+
+        HashMap<Net, HashMap<SiteInst, Map<String, SitePinInst>>> siteInstsOfNet = new HashMap<>();
+        for (Net n: nets){
+            Set<SiteInst> tempSet = n.getSiteInsts();
+            HashMap<SiteInst, Map<String, SitePinInst>> tempMap = new HashMap<>();
+            for (SiteInst si: tempSet) {
+                Map<String,SitePinInst> tempMap2 = si.getSitePinInstMap();
+                tempMap.put(si, tempMap2);
+            }
+            siteInstsOfNet.put(n, tempMap);
+        }
+
+        Collection<ModuleInst> moduleInsts = design.getModuleInsts();
+        Collection<ModuleImpls> modules = design.getModules();
+        HashMap<String, ModuleInst> moduleInstsMap = design.getModuleInstMap();
+
+
+
+//        /*to find unplaced Cells*/
+//        HashSet<Site> unplacedSites = new HashSet<>();
+//        for (Map.Entry<String, Site> entry : allSitesOnDevice.entrySet()) {
+//            if (!entry.getValue().is) unplacedSites.add(entry.getValue());
+//        }
+
 
         //TODO:
         /*to find all unused and type matching sites*/
-        HashMap<String, Site> unusedSites = new HashMap<>();
-        Collection<ArrayList<Site>> allSitesOfSpecificType = sitesOfAllTypeOfSI.values();
-
-        for (ArrayList<Site> list: allSitesOfSpecificType) {
-            for(Site s: list){
-                SitePIP[] sitePIPs = s.getSitePIPs();
-                boolean isUnused = true;
-                for (SitePIP sp: sitePIPs){
-                    if(!sp.getInputPin().getSiteConns().isEmpty()){
-                        isUnused = false;
-                        break;
-                    }
-
-                }
-                //why always null
-                if (isUnused) unusedSites.put(s.getName(), s);
-            }
-        }
+//        HashMap<String, Site> unusedSites = new HashMap<>();
+//        Collection<ArrayList<Site>> tempCol = unusedSitesMapOfNeededType.values();
+//        HashMap<Site, SitePIP[]> Sites = new HashMap<>();
+//        for (ArrayList<Site> arrayList: tempCol) {
+//            for(Site s: arrayList){
+//                SitePIP[] sitePIPs = s.getSitePIPs();
+//                boolean isUnused = true;
+//                Sites.put(s, sitePIPs);
+//                for (SitePIP sp: sitePIPs){
+//                    if(!sp.getInputPin().getSiteConns().isEmpty()){
+//                        isUnused = false;
+//                        break;
+//                    }
+//                }
+//                //why always null
+//                if (isUnused) unusedSites.put(s.getName(), s);
+//            }
+//        }
 
 
 
@@ -164,7 +210,7 @@ public class MyPlacement {
 //        router.routePinsReEntrant(pinsToRoute, true);
 
         // To write out a design
-        design.writeCheckpoint("my_test/dcpfile/output/" + filename);
+        design.writeCheckpoint("my_test/rapidwright_benchmarks_unrouted_v2/benchmarks/spmc/mjpeg_5cores/out_configuration_placed.dcp");
 //        design.writeCheckpoint("my_test/dcpfile/output/1_spi.dcp");
     }
 }
